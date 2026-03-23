d37c5e61b0ed0bad16ae58e28d9ef56990f1e184
mohit <mohitypatil2907@gmail.com>
2026-03-23 12:40:14 +0530

    deployment ready

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ if __name__ == "__main__":
     if not os.path.exists(UTIL_DIR):
         print(f"WARNING: The directory {UTIL_DIR} does not exist!")
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
